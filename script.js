18190dc4166b1ff0571225ff5d4d42431162f7e3
Emmanuel63169 <emmanuel63169@gmail.com>
2024-11-20 19:55:33 -0700

    script and style edit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Start to my JavaScript
+let string1 = "Hello" + " " + "World"
+
+let str1 ="Hello";
+let str2 =" ";
+let str3 ="World"
+let fullstring = str1 +str2 + str3
+
+console.log(fullstring);
